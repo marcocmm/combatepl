010ab14184ebac5044991f5603b68dd5c9da8abf
marcocmm <marco.cmm@gmail.com>
2016-06-28 04:59:55 -0300

    tabuleiro

--- coordenada.pl
+++ coordenada.pl
@@ -5,9 +5,9 @@ getCoordenada(X,Y,[X,Y|Rest]) :- getCoordenada(-2,-2,Rest).
 
 
 %%obtém coordenada para onde a peça irá%%
-getMoveCoordenada(X,Y,Direcao,NovaCoordenada) :- Direcao==-1.
-getMoveCoordenada(X,Y,Direcao,NovaCoordenada) :- Direcao==0, X2 is X-1,getCoordenada(X2,Y,NovaCoordenada).
-getMoveCoordenada(X,Y,Direcao,NovaCoordenada) :- Direcao==1, X2 is X+1,getCoordenada(X2,Y,NovaCoordenada).
-getMoveCoordenada(X,Y,Direcao,NovaCoordenada) :- Direcao==2, Y2 is Y-1,getCoordenada(X,Y2,NovaCoordenada).
-getMoveCoordenada(X,Y,Direcao,NovaCoordenada) :- Direcao==3, Y2 is Y+1,getCoordenada(X,Y2,NovaCoordenada).
+getMoveCoordenada([X,Y|_],Direcao,NovaCoordenada) :- Direcao == -1.
+getMoveCoordenada([X,Y|_],Direcao,NovaCoordenada) :- Direcao==0, X2 is X-1,getCoordenada(X2,Y,NovaCoordenada).
+getMoveCoordenada([X,Y|_],Direcao,NovaCoordenada) :- Direcao==1, X2 is X+1,getCoordenada(X2,Y,NovaCoordenada).
+getMoveCoordenada([X,Y|_],Direcao,NovaCoordenada) :- Direcao==2, Y2 is Y-1,getCoordenada(X,Y2,NovaCoordenada).
+getMoveCoordenada([X,Y|_],Direcao,NovaCoordenada) :- Direcao==3, Y2 is Y+1,getCoordenada(X,Y2,NovaCoordenada).
 
--- tabuleiro.pl
+++ tabuleiro.pl
@@ -19,3 +19,18 @@ size(M, R) :- getNumberOfLines(M, R).
 
 %retorna boolean se a coordenada está entre o tamanho do tabuleiro
 isCoordenadaValida([X, Y], [H | T]) :- X > 0, size([H | T], S), X < S.
+
+
+%%%retorna boolean se há vencedor
+haveWinner(Tabuleiro) :- bandeira(B), getEnemyPiece(B,Bi), getQuantidadeElementosMatriz(B,Tabuleiro,Nb), getQuantidadeElementosMatriz(Bi,Tabuleiro,Nbi),T is Nb + Nbi, not(T == 2).
+
+
+%%retorna boolean se é possível mover a peça para o direção dada
+canMove(Coordenada, Direcao, Tabuleiro, Turno) :- isCoordenadaValida(Coordenada, Tabuleiro), isDirecaoValida(Direcao),
+					getPiece(Coordenada, Tabuleiro, P), bandeira(Ba), bomba(Bo), territorio(T),
+					not(isPieceEquals(P,Ba)), not(isPieceEquals(P,Bo)), not(isPieceEquals(P,T)),
+					isMyPiece(P,Turno),
+					getMoveCoordenada(Coordenada, Direcao, Nc), isCoordenadaValida(Nc,Tabuleiro).
+					
+%%%%obtém a peça de uma coordenada
+getPiece([H|T], Tabuleiro, E) :- getElementoMatriz(Tabuleiro, H, T, E).
